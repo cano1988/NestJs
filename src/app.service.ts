7e8ac0f6c16ff265907007d7db8b867dd515efc7
andres1988 <a.canorave@gmail.com>
2024-07-11 15:28:49 -0500

    fallando

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const saludando: string = 'hello';
+    const saludando: string = 'hello my name is andres';
     return 'Hello World!';
   }
 }
